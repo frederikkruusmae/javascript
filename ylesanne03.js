5df3d7d383efdcad717374ca0fc2a5f369a8edb5
frederikkruusmae <frederikkru@gmail.com>
2024-10-29 10:16:48 +0200

    Update ylesanne03.js

--- ylesanne03.js
+++ ylesanne03.js
@@ -3,3 +3,21 @@
 //03
 
 //Sõiduaeg ja kaugus
+const kilomeetrid = 90;
+const kiirus = 90;
+
+let result = kilomeetrid / kiirus;
+console.log("Sõit kestis: " + result + "h");
+
+//Postitusete kuvamine
+const postitused = 137;
+const postitusilehel = 10;
+
+let vastus = Math.ceil (postitused / postitusilehel);
+let jaak = postitused % postitusilehel;
+
+console.log("Lehekülgi kokku on: " + vastus);
+console.log("Viimasel lehel on " + jaak + " postitust");
+
+//Serveri töökulu
+
