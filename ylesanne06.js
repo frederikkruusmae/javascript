b4263958daa312f0ebba81a4ed683ea213ba4863
frederikkruusmae <frederikkru@gmail.com>
2024-11-29 13:52:25 +0200

    xx

--- ylesanne06.js
+++ ylesanne06.js
@@ -1,3 +1,20 @@
 //Frederik Kruusimäe
 //29.11.2024
 //06
+//POSITIIVNE VÕI NEGATIIVNE
+let number = parseFloat(prompt("Sisesta number:"));
+
+// Kontrolli numbrit switch-case abil
+switch (true) {
+    case (number > 0):
+        console.log("Sisestatud number on positiivne.");
+        break;
+    case (number < 0):
+        console.log("Sisestatud number on negatiivne.");
+        break;
+    case (number === 0):
+        console.log("Sisestatud number on null.");
+        break;
+    default:
+        console.log("Palun sisesta kehtiv number.");
+}
